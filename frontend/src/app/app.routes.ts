270c4482b2609c539fd2a091f157178537cc6317
Susana Cordon <susana.cordon05@gmail.com>
2026-06-10 00:02:11 -0300

    perfil publico casi listo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PerfilPublicoComponent } from './pages/perfil-publico/perfil-publico';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+    path: 'perfil-publico',
+    component: PerfilPublicoComponent
+  }
+];
