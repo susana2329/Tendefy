d1cfcde195e80f4cef64dd0e9dd1c39498cf674d
Susana Cordon <susana.cordon05@gmail.com>
2026-06-11 01:41:35 -0300

    subo parte del login me falta los iconos y una card

---
+++ Frontend/src/app/pages/login/login.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {
+    constructor(private router: Router) {}
+        loginConSpotify(){
+            this.router.navigate(['/home']);
+        }
+        loginConGoogle(){
+            this.router.navigate(['/home'])
+        }
+
+    
+
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+  {
+    path: '',
+    component: LoginComponent
+  }
+
+];
