845f9cfb942b4aec5721ec5ad868878a99b1e664
Juliana Choque <juliana.choque0234@gmail.com>
2026-06-16 18:56:18 -0300

    arreglo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,12 @@ import { Routes } from '@angular/router';
 import { PerfilPersonalComponent } from './page/perfil-personal/perfil-personal';
 
 export const routes: Routes = [
-  {
+ {
     path: 'perfil-personal',
     component: PerfilPersonalComponent
-import { PerfilPublicoComponent } from './pages/perfil-publico/perfil-publico';
-
-
-export const routes: Routes = [
-    {
+  },
+  {
     path: 'perfil-publico',
-    component: PerfilPublicoComponent
+    component: PerfilPersonalComponent
   }
-];
+]
