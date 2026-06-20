3788d7f0ad05a5fe848d6ac865fa89e90c409385
Juliana Choque <juliana.choque0234@gmail.com>
2026-06-20 15:33:17 -0300

    conecto las rutas al servidor en index.js
    -Agrego rutas y middleware al index.js para que los endpoints funcionen

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 const { getMongoDBConnection } = require('./database/conexion');
+const usuarioRouter = require('./routes/usuario.router');
+const spotifyRouter = require('./routes/spotify.routes');
+
+app.use(express.json());
+app.use('/api/usuarios', usuarioRouter);
+app.use('/api', spotifyRouter);
 
 getMongoDBConnection();
 
