71c9b824541a944eb018a1ed4915eb9660046bf5
Susana Cordon <susana.cordon05@gmail.com>
2026-06-22 23:03:01 -0300

    arreglo de archivos y rutas

--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -1,5 +1,27 @@
 const usuarioService = require('../service/usuario.service')
 
+exports.getPerfil = async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log("CONTROLLER - getPerfil por Id", id);
+        const usuarioEncontrado = await usuarioService.getUsuarioById(id);
+
+
+        if (!usuarioEncontrado) {
+            return res.status(404).send(`No fue posible encontrar un usuario con ese ID`);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(JSON.stringify(usuarioEncontrado));
+    } catch (error) {
+        console.log("Error en Controller getperfil", error);
+        res.status(500).send({
+            code: 500,
+            message: "Error al obtener el perfil del usuario"
+        });
+    }
+};
+
+
 exports.readUsuarioById = async (req, res) => {
     try {
         const idParam = req.params.id;
--- backend/routes/usuario.router.js
+++ backend/routes/usuario.router.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const usuarioController = require('../controllers/usuario.controller')
+const authMiddleware = require('../middleware/auth.middleware');
 const router = express.Router();
 
-
 router.get('/:id', usuarioController.readUsuarioById)
-const usuarioController = require('../controllers/usuario.controller');
-const authMiddleware = require('../middleware/auth.middleware');
-
-router.get('/perfil/:id',authMiddleware, usuarioController.getPerfil);
-router.get('/perfil/:id/spotify',authMiddleware, usuarioController.getPerfilSpotify);
+router.get('/perfil/:id', authMiddleware, usuarioController.getPerfil);
+router.get('/perfil/:id/spotify', authMiddleware, usuarioController.getPerfilSpotify);
 
 module.exports = router
